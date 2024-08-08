57f3e8e2705816a933d7d8869f71ea204b1a4e52
Tech-Sayngo <141753444+Tech-Sayngo@users.noreply.github.com>
2024-08-08 17:20:09 +0530

    Update constant.ts

--- client/src/constants/constant.ts
+++ client/src/constants/constant.ts
@@ -4,7 +4,7 @@
 // export const BASE_URL = "http://localhost:5000";
 export const FRONTEND_URL = "https://www.livebookingsayngo247.com";
 
-export const BASE_URL = "https://sayngo-prod-server-ks8c.onrender.com";
+export const BASE_URL = "https://sayngo-prod-server-78h8.onrender.com";
 // export const FRONTEND_URL = "https://staystats.vercel.app";
 
 // export const BASE_URL = "https://www.webcrack.online";
